45c09cf26a206c0035b9df16ae1d205a524fdf12
hankai17 <867614535@qq.com>
2024-01-04 02:42:54 -0500

    add examples

--- examples/test_poll_channel.rs
+++ examples/test_poll_channel.rs
@@ -144,37 +144,100 @@ fn test4() {
     assert_eq!(0, num);
 }
 
-fn main() {
-    test1();
-    //test2();
-    //test3();
-    //test4();
+fn test5() {
+    let poll = Poll::new().unwrap();
+    let mut events = Events::with_capacity(1024);
+    let (tx, rx) = channel::channel();
+    poll.register(&rx, Token(123), Ready::readable(), PollOpt::edge()).unwrap();
+
+    tx.send("hello").unwrap();
+    drop(rx);
+
+    let num = poll.poll(&mut events, Some(Duration::from_millis(300))).unwrap();
+    assert_eq!(0, num);
 }
 
 
+fn test6() {
+    use my_mio::net::{TcpListener, TcpStream};
+    use my_mio::event::Event;
+    use std::thread;
 
+    fn expect_events(poll: &Poll, event_buffer: &mut Events, poll_try_count: usize, mut expected: Vec<Event>) {
+        const MS: u64 = 1_000;
+        for _ in 0..poll_try_count {
+            poll.poll(event_buffer, Some(Duration::from_millis(MS))).unwrap();
+            for event in event_buffer.iter() {
+                let pos_opt = match expected.iter().position(|exp_event| {
+                    (event.token() == exp_event.token()) &&
+                    event.readiness().contains(exp_event.readiness())
+                }) {
+                    Some(x) => Some(x),
+                    None => None,
+                };
+                if let Some(pos) = pos_opt { expected.remove(pos); }
+            }
+            if expected.is_empty() {
+                break;
+            }
+        }
+        assert!(expected.is_empty(), "The following expected events were not found: {:?}", expected);
+    }
 
+    let poll = Poll::new().unwrap();
+    let mut events = Events::with_capacity(1024);
+    let (tx, rx) = channel::channel();
+    let l = TcpListener::bind(&"127.0.0.1:0".parse().unwrap()).unwrap();
+    poll.register(&l, Token(0), Ready::readable(), PollOpt::edge()).unwrap();
+    poll.register(&rx, Token(1), Ready::readable(), PollOpt::edge()).unwrap();
+    tx.send("hello").unwrap(); // rx收到读事件
+    let s1 = TcpStream::connect(&l.local_addr().unwrap()).unwrap(); // l收到读事件
+    poll.register(&s1, Token(2), Ready::readable(), PollOpt::edge()).unwrap(); // 监控vc读
+    thread::sleep(Duration::from_millis(250));
+    expect_events(&poll, &mut events, 2, vec![
+        Event::new(Ready::empty(), Token(0)),
+        Event::new(Ready::empty(), Token(1)),
+    ]);
+}
 
+fn test7() {
+    const ITERATIONS: usize = 20;
+    const THREADS: usize = 5;
+    use std::thread;
 
+    let poll = Poll::new().unwrap();
+    let mut events = Events::with_capacity(1024);
+    for _ in 0..ITERATIONS {
+        let (tx, rx) = channel::channel();
+        poll.register(&rx, Token(0), Ready::readable(), PollOpt::edge()).unwrap();
+        for _ in 0..THREADS {
+            let tx = tx.clone();
+            thread::spawn(move || {
+                thread::sleep(Duration::from_millis(250));
+                tx.send("ping").unwrap();
+            });
+        }
+        let mut recv = 0;
+        while recv < THREADS {
+            let num = poll.poll(&mut events, None).unwrap();
+            if num != 0 {
+                assert_eq!(1, num);
+                assert_eq!(events.get(0).unwrap().token(), Token(0));
+                while let Ok(_) = rx.try_recv() {
+                    recv += 1;
+                }
+            }
+        }
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fn main() {
+    //test1();
+    //test2();
+    //test3();
+    //test4();
+    test5();
+    test6();
+    test7();
+}
 
--- examples/test_tcp.rs
+++ examples/test_tcp.rs
@@ -624,6 +624,47 @@ fn write_error() {
     }
 }
 
+fn write_then_drop() {
+    let a = TcpListener::bind(&"127.0.0.1:0".parse().unwrap()).unwrap();
+    let addr = a.local_addr().unwrap();
+    let mut s = TcpStream::connect(&addr).unwrap();
+    let poll = Poll::new().unwrap();
+
+    a.register(&poll, Token(1), Ready::readable(), PollOpt::edge()).unwrap();
+    s.register(&poll, Token(3), Ready::empty(), PollOpt::edge()).unwrap();
+    let mut events = Events::with_capacity(1024);
+    while events.is_empty() {
+        poll.poll(&mut events, None).unwrap();
+    }
+    assert_eq!(events.len(), 1);
+    assert_eq!(events.get(0).unwrap().token(), Token(1));
+
+    let mut s2 = a.accept().unwrap().0;
+    s2.register(&poll, Token(2), Ready::writable(), PollOpt::edge()).unwrap();
+    let mut events = Events::with_capacity(1024);
+    while events.is_empty() {
+        poll.poll(&mut events, None).unwrap();
+    }
+    assert_eq!(events.len(), 1);
+    assert_eq!(events.get(0).unwrap().token(), Token(2));
+
+    s2.write_all(&[1, 2, 3, 4]).unwrap();
+    //drop(s2);
+    s2.deregister(&poll).unwrap();
+
+    s.reregister(&poll, Token(3), Ready::readable(), PollOpt::edge()).unwrap();
+    let mut events = Events::with_capacity(1024);
+    while events.is_empty() {
+        poll.poll(&mut events, None).unwrap();
+    }
+    assert_eq!(events.len(), 1);
+    assert_eq!(events.get(0).unwrap().token(), Token(3));
+
+    let mut buf = [0; 10];
+    assert_eq!(s.read(&mut buf).unwrap(), 4);
+    assert_eq!(&buf[0..4], &[1, 2, 3, 4]);
+}
+
 fn main() {
     accept();
     connect();
@@ -637,6 +678,7 @@ fn main() {
     bind_twice_bad();
     multiple_writes_imm_success();
     connection_reset_by_peer();
+    write_then_drop();
     write_error();
 }
 
