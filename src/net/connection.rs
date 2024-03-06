c4bb92883aaf683ead84147c476a3dfb529960d8
hankai17 <867614535@qq.com>
2024-03-06 08:53:40 -0500

    ok but strange

--- src/net/connection.rs
+++ src/net/connection.rs
@@ -168,7 +168,7 @@ impl TcpConnection {
 
 impl Drop for TcpConnection {
     fn drop(&mut self) {
-        self.event_loop.lock().unwrap().deregister(&self.sock);
+        //self.event_loop.lock().unwrap().deregister(&self.sock);
         println!("---------------------drop for tcpconnection")
     }
 }
